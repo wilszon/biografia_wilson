a0a6cf8690033cc753063a5871f049a45c1272e0
Wilson Suarez <wsuarez874@unab.edu.co>
2025-09-08 21:21:38 -0500

    Tercera Habilidad

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -280,6 +280,14 @@
         <ul class="habilidades">
             <li>Administración y mantenimiento de equipos de cómputo</li>
             <li>Manejo de Git /GitHub y paquete de Office</li>
+            <li>Trabajo en equipo y resolución de problemas</li>
+            <h3>Conocimientos Técnicos</h3>
+            <ul class= "habilidades">
+                <li>Lenguajes: Python, PHP, Java, Kotlin, SQL y JavaScript</li>
+                <li>Frameworks: Laravel, React</li>
+            </ul>
+        </ul>
+
 
     </section>
 
